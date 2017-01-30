1ce64c257dc8e90837090511f56231c6520a0e31
Geoff Craig <geoff.craig@leankit.com>
2017-01-29 18:14:27 -0600

    Adding roll new character

--- scripts/insults.js
+++ scripts/insults.js
@@ -7,7 +7,7 @@
 module.exports = robot => {
   const insults = [ "You mom was a pale skinned drow", "Your mother takes up more tiles than a gelatinous cube!", "Charisma was OBVIOUSLY your dump stat!", "Ugh what the hell is that all over your face? Oh.. that's just your face." ];
 
-  robot.hear( new RegExp( `insult` ), msg => {
+  robot.respond( new RegExp( `insult` ), msg => {
     msg.reply( `${ msg.random( insults ) }` );
   } );
 };
---
+++ scripts/rollchar.js
@@ -0,0 +1,29 @@
+// Description
+// Roll character
+
+// Commands:
+// hubot roll new character
+
+function charRoll(cb) {
+    var roll = require('roll');
+    roll = new roll();
+
+    var output = roll.roll('4d6b3');
+
+    var newCharRolls = [ ];
+
+    for(i=0; i<6; i++){
+        var rollNum = i+1
+        var CharRoll = roll.roll('4d6b3')
+        newCharRolls.push("Roll #" + rollNum + ": " + CharRoll.calculations[0] + " (Rolled " + CharRoll.rolled + ").");
+    };
+    
+    return newCharRolls;
+}
+
+module.exports = robot => {
+  
+  robot.respond( 'roll new character', msg => {
+    msg.reply( charRoll() );
+  } );
+};
